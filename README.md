368281cfb8e691a2abae8a0404f45aaf5b94b9f7
ndb796 <ndb796@naver.com>
2019-10-19 23:56:09 +0900

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Set Tor Proxy for Windows 10
+### Setup Tor Proxy for Windows 10
 
 * You need to download [Tor Project](https://www.torproject.org/) Windows version.
 * Install using default settings.
@@ -7,4 +7,19 @@ cd C:\Users\User Name\Desktop\Tor Browser\Browser\TorBrowser\Tor
 tor CookieAuthentication 1 ControlPort 9051 HashedControlPassword 16:DB4D0D522B4946F560DBA4D9B0E47C8BA3BC2A3F7CD69C4E30581900BF | more
 </pre>
 
-### 
+###  Usage
+
+* This project requires Node.js
+<pre>
+npm install
+node run.js
+</pre>
+
+### Discussion
+
+* Selenium 및 Puppeteer를 그대로 이용하는 경우 Challenge 없이 Block 처리됩니다.
+* Tor를 이용하여 우회 접속하면, 비로소 Image reCaptcha Challenge가 등장합니다.
+* 3개의 브라우저에서 동시에 수집하도록 하면, 대략 2초에 1개 정도의 이미지가 수집됩니다.
+* 기본적으로 브라우저 개수에 상관없이 정상적으로 reCaptcha v2 화면이 잘 등장합니다.
+* 이 Repository는 단순히 reCaptcha v2의 Image를 수집하는 역할만 수행합니다.
+* Tor를 이용하면 무한히 Image를 Reload할 수 있습니다. (2019-10-19)
---
+++ run.js
@@ -0,0 +1,144 @@
+const puppeteer = require('puppeteer-extra')
+const pluginStealth = require('puppeteer-extra-plugin-stealth')
+const Tor = require('tor-control-promise')
+var fs = require('fs');
+const request = require('request');
+
+// Random number generator
+function rdn (min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+// Delay function for stable data collecting
+function delay( timeout ) {
+  return new Promise(( resolve ) => {
+    setTimeout( resolve, timeout );
+  });
+}
+
+// Download a specific image from url.
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){    
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+// Make folder that contains images if the folder doesn't exist.
+!fs.existsSync('images') && fs.mkdirSync('images');
+
+// Given image URL and annotation, this function saves the image into the proper directory.
+function process(imageUrl, annotation) {
+    if(annotation.indexOf("If") != -1) {
+      const question = annotation.split('If')[0];
+      if(question.indexOf("Select all squares with") != -1) {
+        category = annotation.split('If')[0].split('with ')[1];
+        const dir = './images/' + category + '/';
+        !fs.existsSync(dir) && fs.mkdirSync(dir);
+        const file_length = fs.readdirSync(dir).length;
+        download(imageUrl, dir + 'image' + file_length + '.png', function(){
+          console.log('Image Saved: ' + dir + 'image' + file_length + '.png');
+        });
+      }
+    }
+}
+
+// This is teh solving function for each browser tap.
+async function solve (page) {
+  // Waiting until the reCaptcha anchor appears.
+  await page.waitForFunction(() => {
+    const iframe = document.querySelector('iframe[src*="api2/anchor"]')
+  if (!iframe) return false
+    return !!iframe.contentWindow.document.querySelector('#recaptcha-anchor')
+  })
+
+  // Click check button for receiving reCaptcha image frame.
+  let frames = await page.frames()
+  const recaptchaFrame = frames.find(frame => frame.url().includes('api2/anchor'))
+  const checkbox = await recaptchaFrame.$('#recaptcha-anchor')
+  await checkbox.click({ delay: rdn(30, 150) })
+
+  // Collecting the reCaptcha image infinitely.
+  while(true) {
+    // Waiting until the reCaptcha image frame appears.
+    await page.waitForFunction(() => {
+      const iframe = document.querySelector('iframe[src*="api2/bframe"]')
+      if (!iframe) return false
+      const img = iframe.contentWindow.document.querySelector('.rc-image-tile-wrapper img')
+      return img && img.complete
+    })
+
+    // Obtaining reCaptcha image URL and annotation.
+    const imageUrl = await page.evaluate(() => {
+      const iframe = document.querySelector('iframe[src*="api2/bframe"]')
+      return iframe.contentWindow.document.querySelector('.rc-image-tile-wrapper img').src
+    })
+    const annotation = await page.evaluate(() => {
+      const iframe = document.querySelector('iframe[src*="api2/bframe"]')
+      return iframe.contentWindow.document.querySelector('.rc-imageselect-instructions').textContent
+    })
+
+    // Click the reload button.
+    frames = await page.frames()
+    const imageFrame = frames.find(frame => frame.url().includes('api2/bframe'))
+    const audioButton = await imageFrame.$('#recaptcha-reload-button')
+    await audioButton.click({ delay: rdn(30, 150) })
+
+    // Download the previous image.
+    await delay(rdn(750, 1000));
+    await process(imageUrl, annotation);
+    await delay(rdn(750, 1000));
+  }
+}
+  
+async function run () {
+  // Setup for using Tor
+  const tor = new Tor({
+    host: '127.0.0.1',
+    port: 9051,
+    password: 'tor'
+  })
+
+  // Check whether the Tor is working.
+  try {
+    await tor.connect()
+    await tor.signalNewnym()
+  } catch (e) {
+    console.error(e)
+    console.log('Tor is not working.')
+    process.exit()
+  }
+
+  puppeteer.use(pluginStealth())
+
+  // Creating some browsers and downloading images.
+  const browser1 = await puppeteer.launch({
+    headless: false,
+    args: ['--lang=en', '--window-size=360,500', '--proxy-server=socks5://127.0.0.1:9050', '--window-position=0,0']
+  })
+  const page1 = await browser1.newPage()
+  await page1.setDefaultNavigationTimeout(0)
+  page1.goto('https://www.google.com/recaptcha/api2/demo')
+  solve(page1)
+
+  const browser2 = await puppeteer.launch({
+    headless: false,
+    args: ['--lang=en', '--window-size=360,500', '--proxy-server=socks5://127.0.0.1:9050', '--window-position=0,0']
+  })
+  const page2 = await browser2.newPage()
+  await page2.setDefaultNavigationTimeout(0)
+  page2.goto('https://www.google.com/recaptcha/api2/demo')
+  solve(page2)
+
+  const browser3 = await puppeteer.launch({
+    headless: false,
+    args: ['--lang=en', '--window-size=360,500', '--proxy-server=socks5://127.0.0.1:9050', '--window-position=0,0']
+  })
+  const page3 = await browser3.newPage()
+  await page3.setDefaultNavigationTimeout(0)
+  page3.goto('https://www.google.com/recaptcha/api2/demo')
+  solve(page3)
+}
+
+run()
